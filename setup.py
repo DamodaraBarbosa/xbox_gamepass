58ed13d54a252ed197e905a127025c6811d7d933
Damodara Barbosa <107199898+DamodaraBarbosa@users.noreply.github.com>
2023-09-30 07:51:59 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     url=url_repositorio,
     packages=find_packages(),  # Encontra automaticamente todos os pacotes do projeto
     install_requires=[
-        'time', 'urllib', 'selenium', 'bs4',
+        'urllib', 'selenium', 'bs4',
         'zipfile', 'wget', 'os'
     ]  # Lista de dependências do pacote 
 )
